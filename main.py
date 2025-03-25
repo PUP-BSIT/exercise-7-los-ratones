683a11c16d3a02bf4f18cc66974c395a65ff6da5
reduta-paulbenidict <paulbenidictr@gmail.com>
2025-03-26 00:05:54 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+def get_order_details():
+    """
+    TODO(JohnPaul Rodriguez): 
+    Create a function that accepts order details.
+    """
+    pass
+
+def get_customer_details():
+    """
+    TODO(Jedi Duncan Gonot): 
+    Create a function that accepts customer details.
+    """
+    pass
+
+def calculate_grand_total():
+    """
+    TODO(Kenji Enishi Campos): 
+    Create a function that calculate the grand total of items.
+    """
+    pass
+
+def display_receipt():
+    """
+    TODO(Dion Manicio):
+    Create a function that displays the order information.
+    """
+    pass
+
+def main_function():
+    """
+    TODO(Paul Benidict Reduta): 
+    Create a function that integrates all the other functions.
+    """
+    pass
+
+main_function()
